83e517bf0e2dcb4fb5ff7ea6ffcd9864299f9de6
biblepay <contact@biblepay.org>
2021-08-29 08:49:50 -0500

    0.17.0.3e - Initial Commit - Merge in BIBLEPAY branded NSIS installer for windows and update icons - Merge in bitcoins patch to make the code compile on Darwin14 (MAC), theoretically allowing us to compile on BigSur (tested on cross-compile with ubuntu 20.04->Darwin14 successfully)

---
+++ src/crypto/RandomX/buildwin64/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.4.0-66-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-66-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/biblepay-evolution/src/crypto/RandomX/buildwin64/win64.cmake")
+
+set(CMAKE_SYSTEM "Windows")
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/biblepay-evolution/src/crypto/RandomX/src/argon2_avx2.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/argon2_avx2.c.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/argon2_core.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/argon2_core.c.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/argon2_ref.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/argon2_ref.c.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/argon2_ssse3.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/argon2_ssse3.c.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/blake2/blake2b.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/blake2/blake2b.c.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/jit_compiler_x86_static.S" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/jit_compiler_x86_static.S.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/reciprocal.c" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/reciprocal.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/biblepay-evolution/src/crypto/RandomX/src/aes_hash.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/aes_hash.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/allocator.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/allocator.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/assembly_generator_x86.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/assembly_generator_x86.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/blake2_generator.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/blake2_generator.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/bytecode_machine.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/bytecode_machine.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/cpu.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/cpu.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/dataset.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/dataset.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/instruction.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/instruction.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/instructions_portable.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/instructions_portable.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/jit_compiler_x86.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/jit_compiler_x86.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/randomx.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/randomx.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/soft_aes.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/soft_aes.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/superscalar.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/superscalar.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/virtual_machine.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/virtual_machine.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/virtual_memory.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/virtual_memory.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/vm_compiled.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/vm_compiled.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/vm_compiled_light.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/vm_compiled_light.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/vm_interpreted.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/vm_interpreted.cpp.obj"
+  "/biblepay-evolution/src/crypto/RandomX/src/vm_interpreted_light.cpp" "/biblepay-evolution/src/crypto/RandomX/buildwin64/CMakeFiles/randomx.dir/src/vm_interpreted_light.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
