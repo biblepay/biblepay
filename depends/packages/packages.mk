4cad9524e57d2b4feb4318a40939b680ae754bed
Bible Pay <rob@usglobaldata.com>
2023-04-15 17:23:31 -0500

    0.17.2.9j

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,7 +3,7 @@ packages:=boost openssl libevent zeromq gmp bls-dash backtrace cmake
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
@@ -13,8 +13,6 @@ wallet_packages=bdb
 upnp_packages=miniupnpc
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
-
-$(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
